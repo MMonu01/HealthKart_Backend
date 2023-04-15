419e1d05d7d79c74e93578a34177b2954a5b4e41
MMonu01 <mdmonu2021@gmail.com>
2023-04-15 15:52:19 +0530

    file

--- routes/User.routes.js
+++ routes/User.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { UserModel } = require("../Model/User.model");
+const { UserModel } = require("../model/User.model");
 
 const UserRouter = express.Router();
 
